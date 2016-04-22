3455ea63303449ee7a3fe092601b97e6605c2bdd
iulia0103 <iulia.leancu@gmail.com>
2016-04-22 11:49:38 +0200

    all tests completed

--- spec/features/todos_spec.rb
+++ spec/features/todos_spec.rb
@@ -1,20 +1,73 @@
 require 'rails_helper'
 
 feature 'Manage todos in the list', :js => true do
+
+def create_todo(title)
+  visit root_path
+  fill_in 'todo_title', with: title
+  page.execute_script("$('form#new_todo').submit();")
+end
+
   scenario 'We can create new tasks' do
     title = "catch capybaras"
-    visit root_path
-    fill_in 'todo_title', with: "catch capybaras"
-    page.execute_script("$('form#new_todo').submit();")
+    create_todo(title)
     expect(page).to have_content("catch capybaras")
   end
 
   scenario 'Counter updates when creating new tasks' do
-  visit root_path
-  title = "catch capybaras"
-  fill_in 'todo_title', with: "catch capybaras"
-  page.execute_script("$('form#new_todo').submit();")
-  expect( page.find(:css, 'span#todo-count').text ).to eq "1"
+    title = "catch capybaras"
+    create_todo(title)
+    expect( page.find(:css, 'span#todo-count').text ).to eq "1"
   end
 
+  scenario 'completed counter updates when completing tasks' do
+    title = "catch capybaras"
+    create_todo(title)
+    expect( page.find(:css, 'span#completed-count').text ).to eq "0"
+    check "todo-1"
+    sleep 2
+    expect( page.find(:css, 'span#todo-count').text ).to eq "0"
+    expect( page.find(:css, 'span#completed-count').text ).to eq "1"
+  end
+
+  scenario 'total count, completed count, todo count' do
+    title = "catch capybaras"
+    create_todo(title)
+    expect( page.find(:css, 'span#completed-count').text ).to eq "0"
+    check "todo-1"
+    sleep 2
+    expect( page.find(:css, 'span#todo-count').text ).to eq "0"
+    expect( page.find(:css, 'span#completed-count').text ).to eq "1"
+
+    create_todo(title)
+    expect( page.find(:css, 'span#completed-count').text ).to eq "1"
+    check "todo-2"
+    sleep 2
+    expect( page.find(:css, 'span#todo-count').text ).to eq "0"
+    expect( page.find(:css, 'span#completed-count').text ).to eq "2"
+
+    create_todo(title)
+    expect( page.find(:css, 'span#total-count').text ).to eq "3"
+  end
+
+  scenario 'total count, completed count, todo count' do
+    title = "catch capybaras"
+    create_todo(title)
+    expect( page.find(:css, 'span#completed-count').text ).to eq "0"
+    check "todo-1"
+    sleep 2
+    expect( page.find(:css, 'span#todo-count').text ).to eq "0"
+    expect( page.find(:css, 'span#completed-count').text ).to eq "1"
+
+    create_todo(title)
+    expect( page.find(:css, 'span#completed-count').text ).to eq "1"
+    check "todo-2"
+    sleep 2
+    expect( page.find(:css, 'span#todo-count').text ).to eq "0"
+    expect( page.find(:css, 'span#completed-count').text ).to eq "2"
+
+    create_todo(title)
+    click_link("clean-up")
+    expect( page.find(:css, 'span#total-count').text ).to eq "1"
+  end
 end
